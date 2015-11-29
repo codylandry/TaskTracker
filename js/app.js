03578d34f1558a07e88cb908d84c1ef0e2a45b3e
Cody Landry <Cody.landry@me.com>
2015-11-29 11:15:57 -0500

    added sort ordering by date

--- js/app.js
+++ js/app.js
@@ -3,44 +3,48 @@
  */
 
 var taskTableHTML = '<table class="table"><col width="10%"><col width="70%"><col width="20%">';
-var tasks = [
-    {status: false,
-     description: "Clean the house",
-     due_date: "11-15-2015"},
-    {status: false,
-     description: "Mow the yard",
-     due_date: "11-15-2015"},
-    {status: false,
-     description: "Take medicine",
-     due_date: "11-15-2015"},
-    {status: false,
-     description: "Cook Dinner",
-     due_date: "11-15-2015"}
-];
+var tasks = [];
 
 
+function newTask () {
+    if ($('#newTaskDesc').val() == '') {alert('Please Enter a Description.'); return}
+    if ($('#newTaskDate').val() == '') {alert('Please Enter a Due Date.'); return}
 
-function createTblRow (element, index) {
-    return "<tr><td><input id='taskChkBox" + index + "' type='checkbox'></td>" +
-                        "<td>" + element.description +"</td>" +
-                        "<td>" + element.due_date + "</td></tr>";
+    tasks.push({status: false,
+              description: $('#newTaskDesc').val(),
+              due_date: new Date($('#newTaskDate').val())});
+
+    buildTaskTable();
+
+    $('#newTaskDesc').text('');
+    $('#newTaskDate').text('');
+    $('#newTaskBtn').click(newTask);
+    $('#newTaskDate').datepicker();
+    $('.taskChkBox').click(function(){
+        $(this).parent().siblings().toggleClass('strike-through');
+    });
+
 }
 
 
 function buildTaskTable () {
     var taskTableHTML = '<table class="table"><col width="10%"><col width="70%"><col width="20%">';
-    tasks.forEach(function(v, i, a) {
+
+    var sorted_tasks = tasks.sort(function(a, b){return new Date(a.due_date) - new Date(b.due_date);});
+
+    sorted_tasks.forEach(function(v, i, a) {
         taskTableHTML += "<tr><td><input class='taskChkBox' data-index=" + i + " id='taskChkBox" + i + "' type='checkbox'></td>" +
                         "<td class='taskDescription'>" + v.description +"</td>" +
-                        "<td class='taskDueDate'>" + v.due_date + "</td></tr>";});
+                        "<td class='taskDueDate'>" + v.due_date.toDateString() + "</td></tr>";});
     taskTableHTML += "<tr>" +
                         "<td>" +
-                        "<input type='submit'></td>" +
-                        "<td><input type='text'></td>" +
+                        "<input id='newTaskBtn' type='submit'></td>" +
+                        "<td><input id='newTaskDesc' type='text'></td>" +
                         "<td><input id='newTaskDate' type='text'></td>" +
                         "</tr></table>";
     $('#task-table').html(taskTableHTML);
 }
+
 
 $(document).ready(function(){
 
@@ -48,6 +52,6 @@ $(document).ready(function(){
     $('#newTaskDate').datepicker();
     $('.taskChkBox').click(function(){
         $(this).parent().siblings().toggleClass('strike-through');
-        updateTask($(this).attr('data-index'));
     });
+    $('#newTaskBtn').click(newTask);
 });
